c9a5090968c79d93fad56b5d1f072a372c4d0cc7
Nathan Braswell <miloigni@gmail.com>
2012-11-29 23:02:43 -0500

    Walking animation works at the correct speed now, one frame per tile, resetting if not moving.

--- game.py
+++ game.py
@@ -47,50 +47,51 @@ class Person:
 		self._inbetweanMove = False		#Inbetween move is a bool that tells us if we're still moving
 
 		self.faceingDirection = (0,-1)
+		self.whichStep = 0
 
 	#This function tells the character how far to move, starting the move
 	def go(self, posTup):
 		self._movingPos[0], self._movingPos[1] = posTup	#Assign our movement variables to the input
 		self._inbetweanMove = True
 
-	#This function gets the index into the tile list approite for our direction and drawingStage
-	def getFrameIndex(self, drawingStage):
+	#This function gets the index into the tile list approite for our direction and self.whichStep
+	def getFrameIndex(self):
 		if self.faceingDirection == (0,-1):
-			if self._inbetweanMove:				#Only use the other move frame's for each drawingStage if we are moving
-				if drawingStage == 1:
+			if self._inbetweanMove:				#Only use the other move frame's for each self.whichStep if we are moving
+				if self.whichStep == 1:
 					return(2)
-				elif drawingStage == 2:
+				elif self.whichStep == 2:
 					return(0)
-				elif drawingStage == 3:
+				elif self.whichStep == 3:
 					return(10)
-			return(0)							#Must be drawingStage 0
+			return(0)							#Must be self.whichStep 0
 		elif self.faceingDirection == (0,1):
-			if self._inbetweanMove:				#Only use the other move frame's for each drawingStage if we are moving
-				if drawingStage == 1:
+			if self._inbetweanMove:				#Only use the other move frame's for each self.whichStep if we are moving
+				if self.whichStep == 1:
 					return(8)
-				elif drawingStage == 2:
+				elif self.whichStep == 2:
 					return(5)
-				elif drawingStage == 3:
+				elif self.whichStep == 3:
 					return(11)
-			return(5)							#Must be drawingStage 0
+			return(5)							#Must be self.whichStep 0
 		elif self.faceingDirection == (-1,0):
-			if self._inbetweanMove:				#Only use the other move frame's for each drawingStage if we are moving
-				if drawingStage == 1:
+			if self._inbetweanMove:				#Only use the other move frame's for each self.whichStep if we are moving
+				if self.whichStep == 1:
 					return(3)
-				elif drawingStage == 2:
+				elif self.whichStep == 2:
 					return(6)
-				elif drawingStage == 3:
+				elif self.whichStep == 3:
 					return(9)
-			return(6)							#Must be drawingStage 0
+			return(6)							#Must be self.whichStep 0
 		elif self.faceingDirection == (1,0):
-			if self._inbetweanMove:				#Only use the other move frame's for each drawingStage if we are moving
-				if drawingStage == 1:
+			if self._inbetweanMove:				#Only use the other move frame's for each self.whichStep if we are moving
+				if self.whichStep == 1:
 					return(4)
-				elif drawingStage == 2:
+				elif self.whichStep == 2:
 					return(1)
-				elif drawingStage == 3:
+				elif self.whichStep == 3:
 					return(7)
-			return(1)							#Must be drawingStage 0
+			return(1)							#Must be self.whichStep 0
 
 		return(0)
 
@@ -112,18 +113,27 @@ class Person:
 			if (self.position[0] >= sectionPos[0]) and (self.position[0] <= (sectionPos[0]+ sectionSize[0])) and (self.position[1] >= sectionPos[1]) and (self.position[1] <= (sectionPos[1]+ sectionSize[1])):
 				#Calculate where on the screen we should draw
 				blitPos = ( (tileSize[0]*(self.previousPosition[0]-sectionPos[0])) + sectionPixelOffset[0] + (self.ownPixelOffset[0]*drawingStage), (tileSize[1]*(self.previousPosition[1]-sectionPos[1])) + sectionPixelOffset[1] + (self.ownPixelOffset[1]*drawingStage) )
-				surface.blit(self.tileList[self.getFrameIndex(drawingStage)], blitPos)
+				surface.blit(self.tileList[self.getFrameIndex()], blitPos)
 
 		else: #If this is a major frame, i.e. drawing stage 0
 
 			if self._movingPos[0] == 0 and self._movingPos[1] == 0:	#If that was the last stage of the move, then set our moving status to False
-				self._inbetweanMove = False		
+				if self._inbetweanMove == False:						#If we're not moving
+					self.whichStep = 0									#Reset the which step counter
+				else:
+					self.whichStep += 1 								#Go to next step, if over 3 go back to 0
+					if self.whichStep > 3:
+						self.whichStep = 0
+				
+				self._inbetweanMove = False								#set moving to false b/c we're done with this go. If a go happens next frame, it will be changed to true before we get back to the whichStep, so it still works
+
+
 			self.previousPosition[0], self.previousPosition[1] = self.position	#reset our previousPosition to our current position
 			#If our position is within the section, found by sectionPos+sectionSize, then we draw ourselves
 			if (self.position[0] >= sectionPos[0]) and (self.position[0] <= (sectionPos[0]+ sectionSize[0])) and (self.position[1] >= sectionPos[1]) and (self.position[1] <= (sectionPos[1]+ sectionSize[1])):
 				#Calculate where on the screen we should draw
 				blitPos = ( (tileSize[0]*(self.position[0]-sectionPos[0])) + sectionPixelOffset[0] + (self.ownPixelOffset[0]*drawingStage), (tileSize[1]*(self.position[1]-sectionPos[1])) + sectionPixelOffset[1] + (self.ownPixelOffset[1]*drawingStage) )
-				surface.blit(self.tileList[self.getFrameIndex(drawingStage)], blitPos)
+				surface.blit(self.tileList[self.getFrameIndex()], blitPos)
 			
 		
 
